c7a04c69806de434dd9812659bead4a67684df99
shaoshao9669 <aldnoahzeroclassic@hotmail.com>
2024-03-11 14:54:46 +0800

    adjust main.cpp

--- SourceFiles/main.cpp
+++ SourceFiles/main.cpp
@@ -4,12 +4,21 @@
 #include "UniquePaths3.h"
 #include "ReverseLinkedList.h"
 #include "WordSearch.h"
+#include "WordSearch2.h"
 #include "NumberOfIslands.h"
 #include "SurroundedRegions.h"
 #include "AddStrings.h"
 #include "AddTwoNumbers.h"
 #include "CombinationSum.h"
 #include "CombinationSum2.h"
+#include "CombinationSum3.h"
+#include "MinCostClimbingStairs.h"
+#include "TrappingRainWater.h"
+#include "TrappingRainWater2.h"
+#include "MaximumNumberofPointsFromGridQueries.h"
+#include "RottingOranges.h"
+#include "DetonateTheMaximumBombs.h"
+#include "NumberOfProvinces.h"
 
 using namespace TwoSum;
 using namespace UniquePaths;
@@ -17,12 +26,21 @@ using namespace UniquePaths2;
 using namespace UniquePaths3;
 using namespace ReversedLinkedList;
 using namespace WordSearch;
+using namespace WordSearch2;
 using namespace NumberOfIslands;
 using namespace SurroundedRegions;
 using namespace AddStrings;
 using namespace AddTwoNumbers;
 using namespace CombinationSum;
 using namespace CombinationSum2;
+using namespace CombinationSum3;
+using namespace MinCostClimbingStairs;
+using namespace TrappingRainWater;
+using namespace TrappingRainWater2;
+using namespace MaximumNumberOfPointsFromGridQueries;
+using namespace RottingOranges;
+using namespace DetonateTheMaximumBombs;
+using namespace NumberOfProvinces;
 
 int main()
 {
@@ -32,11 +50,20 @@ int main()
 //	UniquePaths2::Solution::test();
 //	UniquePaths3::Solution::test();
 //	WordSearch::Solution::test();
+//	WordSearch2::Solution::test();
 //	SurroundedRegions::Solution::test();
 //	AddStrings::Solution::test();
 //	AddTwoNumbers::Solution::test();
 //	CombinationSum::Solution::test();
-	CombinationSum2::Solution::test();
+//	CombinationSum2::Solution::test();
+//	CombinationSum3::Solution::test();
+//	MinCostClimbingStairs::Solution::test();
+//	TrappingRainWater::Solution::test();
+//	TrappingRainWater2::Solution::test();
+//	MaximumNumberOfPointsFromGridQueries::Solution::test();
+//	RottingOranges::Solution::test();
+//	DetonateTheMaximumBombs::Solution::test();
+	NumberOfProvinces::Solution::test();
 
 	return 0;
 }
